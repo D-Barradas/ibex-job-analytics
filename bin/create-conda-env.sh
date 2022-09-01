51bbb6a097c68cae7c479efe731168f247ccca25
Didier Barradas Bautista <didier.barradas@gmail.com>
2022-09-01 13:16:49 +0300

    Update create-conda-env.sh

--- bin/create-conda-env.sh
+++ bin/create-conda-env.sh
@@ -2,6 +2,6 @@
 
 # create the conda environment
 export ENV_PREFIX=$PWD/env
-conda env create --prefix $ENV_PREFIX --file environment.yml --force
-conda activate $ENV_PREFIX
+mamba env create --prefix $ENV_PREFIX --file environment.yml --force
+mamba activate $ENV_PREFIX
 . postBuild
