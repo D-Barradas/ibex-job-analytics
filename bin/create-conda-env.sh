d4e3d61e664c36bfca6bc8c57ec00363702207a3
barradd <didier.barradasbautista@kaust.edu.sa>
2022-09-04 13:56:11 +0300

    Update in the sbatch files

--- bin/create-conda-env.sh
+++ bin/create-conda-env.sh
@@ -3,5 +3,5 @@
 # create the conda environment
 export ENV_PREFIX=$PWD/env
 mamba env create --prefix $ENV_PREFIX --file environment.yml --force
-mamba activate $ENV_PREFIX
-. postBuild
+#mamba activate $ENV_PREFIX
+#. postBuild
